3111482d64c61d33b8b2081ade335e72164a0eca
유민우 <zx12797@naver.com>
2024-09-04 15:55:41 +0900

    [faet] searchViewController
    검색화면 만드는 중

--- u_no/View/SearchView/SearchViewController.swift
+++ u_no/View/SearchView/SearchViewController.swift
@@ -6,8 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchViewController: UIViewController {
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundImage = UIImage()
+//        searchBar.placeholder = "검색어를 입력하세요"
+        searchBar.tintColor = .lightGray
+        searchBar.searchTextField.textColor = .black
+        searchBar.searchTextField.layer.borderColor = UIColor.lightGray.cgColor
+        searchBar.searchTextField.layer.borderWidth = 1.0
+        searchBar.searchTextField.layer.cornerRadius = 10
+        searchBar.searchTextField.clipsToBounds = true
+        
+        let placeholderAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.lightGray
+        ]
+        searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "검색어를 입력하세요", attributes: placeholderAttributes)
+        
+//        if let glassIconView = searchBar.searchTextField.leftView as? UIImage {
+//            glassIconView.image = glassIconView.image?.withRenderingMode(.alwaysTemplate)
+//            glassIconView.tintColor = .lightGray
+//        }
+        return searchBar
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
