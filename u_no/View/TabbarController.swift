afb18e0bb9f395720a3fafa102301f5737107287
Lime <75249291+LikelyLime@users.noreply.github.com>
2024-09-05 15:08:46 +0900

    Revert "Revert "Seung hyun""

---
+++ u_no/View/TabbarController.swift
@@ -0,0 +1,39 @@
+//
+//  TabbarController.swift
+//  u_no
+//
+//  Created by t2023-m0117 on 9/4/24.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabbar()
+    }
+    
+    private func setupTabbar() {
+        
+        let first = GraphViewController()
+        let second = SearchViewController()
+        let third = FavoritesController()
+        let four = SettingController()
+        
+        first.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        second.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        third.tabBarItem = UITabBarItem(title: "즐겨찾기", image: UIImage(systemName: "bookmark.fill"), tag: 2)
+        four.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "gearshape.fill"), tag: 3)
+        
+        viewControllers = [first, second, third, four]
+        
+        tabBar.backgroundColor = .white
+        tabBar.tintColor = UIColor(red: 111/255, green: 230/255, blue: 102/255, alpha: 1)
+        tabBar.unselectedItemTintColor = .lightGray
+        tabBar.isTranslucent = false
+        
+        selectedIndex = 0
+        
+    }
+}
